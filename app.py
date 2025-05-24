512039a7a592083835d5174a12292812cccecfa9
helloween777 <i2422394@continental.edu.pe>
2025-05-24 02:39:53 -0500

    Update app.py
    prueba

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import streamlit as st
+from supabase import create_client
+
+@st.cache_resource
+def init_supabase():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_supabase()
+
 response = supabase.table("predicciones_inundacion").select("*").execute()
 st.write(response.data)
 
